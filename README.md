3bd1e08aaffed4b04ec8298fd0297baa645c8b1b
Yukenigon <timothee.arnauld1@gmail.com>
2017-09-14 12:45:36 +0200

    Beautify

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 http://www.cryptlib.com/
 https://www.cryptopp.com/
+
+g++ sha256.cpp main.cpp -o sha256_example && ./sha256_example
--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ using std::string;
 using std::cout;
 using std::endl;
  
-int main(int argc, char *argv[])
-{
-        string input = "grape";
-            string output1 = sha256(input);
+int main(int argc, char *argv[]){
+    string input = "grape";
+    string output1 = sha256(input);
              
-                cout << "sha256('"<< input << "'):" << output1 << endl;
-                    return 0;
+    cout << "sha256('"<< input << "'):" << output1 << endl;
+    return 0;
 }
