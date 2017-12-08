bc69adcd89a646229123040eb6474095dfda0f8b
Yukenigon <timothee.arnauld1@gmail.com>
2017-12-08 14:12:54 +0100

    Help menu

--- src/parseargs.cpp
+++ src/parseargs.cpp
@@ -38,12 +38,15 @@ Datas ParseArgs::parse(){
 			case '?':
 			if (optopt == 't' || optopt == 's' || optopt == 'c' || optopt == 'l'){
 				fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				std::cout << "-h to see usages" << std::endl;
 			}
 			else if (isprint (optopt)){
 				fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				std::cout << "-h to see usages" << std::endl;
 			}
 			else{
 				fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				std::cout << "-h to see usages" << std::endl;
 			}
 			default:
 			abort ();
@@ -56,6 +59,7 @@ Datas ParseArgs::parse(){
 			return d;
 		}else{
 			printf ("Non-option argument %s\n", vargs[i]);
+			std::cout << "-h to see usages" << std::endl;
 		}
 	}
 	this->d.letsgo = false;
@@ -63,5 +67,8 @@ Datas ParseArgs::parse(){
 }
 
 void ParseArgs::show_usage(){
-	std::cout << "hello world" << std::endl;
+	std::cout << "SHA256-Bruteforcer usages:" << std::endl;
+	std::cout << "-h: display usages" << std::endl;
+	std::cout << "-c hello: give the SHA256 hash of 'hello'" << std::endl;
+	std::cout << "SHA256_HASH: retrieve the passphrase" << std::endl;
 }
